2819d0277558efdc09130a685cf1b5ebf20f1a01
rainbow_bamboo <ada@tinyproblems.com>
2020-03-14 20:09:56 -0400

    bumped version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject chartwell-cljs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject chartwell-cljs "0.1.0"
+  :description "This library generates reagent components which can be used to create charts when paired with the font-family, FF Chartwell."
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
